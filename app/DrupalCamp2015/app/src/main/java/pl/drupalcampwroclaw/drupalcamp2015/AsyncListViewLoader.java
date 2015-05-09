e9b3133f8bb7c90b8a00372a47655f20eaa2aec1
Robert Kandzia <robert.kandzia@droptica.pl>
2015-05-10 00:15:48 +0200

    Issue #0: Init sessions.

---
+++ app/DrupalCamp2015/app/src/main/java/pl/drupalcampwroclaw/drupalcamp2015/AsyncListViewLoader.java
@@ -0,0 +1,14 @@
+package pl.drupalcampwroclaw.drupalcamp2015;
+
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+
--- app/DrupalCamp2015/app/src/main/java/pl/drupalcampwroclaw/drupalcamp2015/MainActivity.java
+++ app/DrupalCamp2015/app/src/main/java/pl/drupalcampwroclaw/drupalcamp2015/MainActivity.java
@@ -1,16 +1,25 @@
 package pl.drupalcampwroclaw.drupalcamp2015;
 
-import android.app.ActionBar;
-import android.app.Application;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    private SessionAdapter sessions_list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,18 @@ public class MainActivity extends ActionBarActivity {
 
         // Tabs.
         this.onCreateTabs();
+
+        // Load sessions.
+        this.onLoadSessions();
+    }
+
+    private void onLoadSessions() {
+        sessions_list  = new SessionAdapter(new ArrayList<Session>(), this);
+        ListView lView = (ListView) findViewById(R.id.listSession);
+
+        lView.setAdapter(sessions_list);
+
+        (new AsyncListViewLoader()).execute();
     }
 
     /**
@@ -65,7 +86,7 @@ public class MainActivity extends ActionBarActivity {
             // Action with ID action_refresh was selected.
             case R.id.action_refresh:
                 // @TODO: Refresh data (file JSON).
-                String refresh_data_message = getString(R.string.refresh_data);
+                String refresh_data_message = getString(R.string.refresh_data_message);
                 Toast.makeText(this, refresh_data_message, Toast.LENGTH_SHORT).show();
                 break;
             default:
@@ -75,4 +96,50 @@ public class MainActivity extends ActionBarActivity {
         return super.onContextItemSelected(item);
     }
 
+    private class AsyncListViewLoader extends AsyncTask<String, Void, List<Session>> {
+        private final ProgressDialog dialog = new ProgressDialog(MainActivity.this);
+
+        @Override
+        protected void onPostExecute(List<Session> result) {
+            super.onPostExecute(result);
+
+            // Hide dialog.
+            dialog.dismiss();
+
+            sessions_list.setItemList(result);
+            sessions_list.notifyDataSetChanged();
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            // Show dialog.
+            String message = getString(R.string.loading_sessions_message);
+            dialog.setMessage(message);
+            dialog.show();
+        }
+
+        @Override
+        protected List<Session> doInBackground(String... params) {
+            List<Session> result = new ArrayList<Session>();
+
+            try {
+
+                // @TODO: Load data with JSON.
+
+                for(int i = 1; i < 100; i++) {
+                    result.add(new Session("Session " + i , "John Smith", "11:00 - 12:00", "Room " + i, "en"));
+                }
+
+
+                return result;
+            }
+            catch(Throwable t) {
+                t.printStackTrace();
+            }
+            return null;
+        }
+
+    }
 }
