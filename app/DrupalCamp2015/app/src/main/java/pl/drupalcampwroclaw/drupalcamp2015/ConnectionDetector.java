f6e654be81666b0b1b5844314b475e1e5a0ba8dc
Robert Kandzia <robert.kandzia@droptica.pl>
2015-05-13 23:04:14 +0200

    Issue #0: Code review.

--- app/DrupalCamp2015/app/src/main/java/pl/drupalcampwroclaw/drupalcamp2015/ConnectionDetector.java
+++ app/DrupalCamp2015/app/src/main/java/pl/drupalcampwroclaw/drupalcamp2015/ConnectionDetector.java
@@ -11,9 +11,6 @@ public class ConnectionDetector {
 
     private Context _context;
 
-    // Flag - status connection Internet.
-    Boolean statusInternet = false;
-
     public ConnectionDetector(Context context){
         this._context = context;
     }
@@ -34,50 +31,6 @@ public class ConnectionDetector {
         }
 
         return false;
-    }
-
-    public boolean checkStatusInternet() {
-        // get Internet status
-        statusInternet = this.isConnectingToInternet();
-
-        // check for Internet status
-        if (statusInternet) {
-            //showAlertDialog(MainActivity.this, "Internet Connection", "You have internet connection", true);
-            return true;
-        }
-        else {
-            //showAlertDialog(AndroidDetectInternetConnectionActivity.this, "No Internet Connection", "You don't have internet connection.", false);
-            return false;
-        }
-    }
-
-    /**
-     * Function to display simple Alert Dialog
-     * @param context - application context
-     * @param title - alert dialog title
-     * @param message - alert message
-     * @param status - success/failure (used to set icon)
-     * */
-    public void showAlertDialog(Context context, String title, String message, Boolean status) {
-        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
-
-        // Setting Dialog Title
-        alertDialog.setTitle(title);
-
-        // Setting Dialog Message
-        alertDialog.setMessage(message);
-
-        // Setting alert dialog icon
-        // alertDialog.setIcon((status) ? R.drawable.success : R.drawable.fail);
-
-        // Setting OK Button
-        alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-            }
-        });
-
-        // Showing Alert Message
-        alertDialog.show();
     }
 
 }
